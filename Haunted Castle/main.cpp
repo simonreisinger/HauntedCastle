4485e735772c39df5b18e5952bfcaf89db065397
Michael Pointner <1427791@gmail.com>
2018-11-24 14:34:16 +0100

    FreeType removed

--- Haunted Castle/main.cpp
+++ Haunted Castle/main.cpp
@@ -33,8 +33,6 @@
 
 
 
-#include <ft2build.h>
-#include FT_FREETYPE_H  
 
 //-------Loading PhysX libraries (32bit only)----------//
 
@@ -414,21 +412,6 @@ int main(int argc, char** argv)
 			glEnable(GL_DEBUG_OUTPUT_SYNCHRONOUS);
 		}
 	#endif
-
-	// FreeType
-	FT_Library ft;
-	if (FT_Init_FreeType(&ft))
-		std::cout << "ERROR::FREETYPE: Could not init FreeType Library" << std::endl;
-
-	FT_Face face;
-	if (FT_New_Face(ft, "fonts/arial.ttf", 0, &face))
-		std::cout << "ERROR::FREETYPE: Failed to load font" << std::endl;
-
-	FT_Set_Pixel_Sizes(face, 0, 48);
-
-	if (FT_Load_Char(face, 'X', FT_LOAD_RENDER))
-		std::cout << "ERROR::FREETYTPE: Failed to load Glyph" << std::endl;
-
 
 
 	init();
